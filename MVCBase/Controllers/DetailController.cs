8afbb7905f2cbd4de39700cb16efbf96d92965d7
vingi <vingi520@msn.com>
2012-11-14 02:00:26 +0800

    finish
    Signed-off-by: vingi <vingi520@msn.com>

---
+++ MVCBase/Controllers/DetailController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBase.Domain.Entity;
+using MVCBase.DAL;
+using NHibernate;
+
+namespace MVCBase.Controllers
+{
+    public class DetailController : Controller
+    {
+        //
+        // GET: /Detail/
+
+        public ActionResult News(int? id)
+        {
+            News dal = new News();
+            Ba_News model = dal.GetSingleNewsById(id.HasValue ? (int)id : 1);
+
+            return View(model);
+        }
+
+        public ActionResult Medium(int? id)
+        {
+            Medium dal = new Medium();
+            Ba_Medium model = dal.GetSinglemediumById(id.HasValue ? (int)id : 1);
+
+            return View(model);
+        }
+
+    }
+}
--- MVCBase/Controllers/ListController.cs
+++ MVCBase/Controllers/ListController.cs
@@ -19,6 +19,19 @@ namespace MVCBase.Controllers
             News dal = new News();
             IList<Ba_News> model = dal.GetNews(id.HasValue ? (int)id : 1);
 
+            IList<Ba_News> allmodel = dal.GetNews();
+            int pagenum = 1;
+            if (allmodel.Count % 3 == 0)
+                pagenum = allmodel.Count / 3;
+            else
+                pagenum = allmodel.Count / 3 + 1;
+            int currentpage = id.HasValue ? (int)id : 1;
+            Common.HtmlPagerControl page = new Common.HtmlPagerControl(pagenum, 3, 7);
+            page.CurrentPage = currentpage;
+            ViewBag.currentpage = currentpage;
+            page.ClickEvent = "onclick=\"topage(this);\"";
+            ViewBag.pageinfo = page.Render();
+
             return View(model);
         }
 
@@ -26,6 +39,19 @@ namespace MVCBase.Controllers
         {
             Medium dal = new Medium();
             IList<Ba_Medium> model = dal.Getmedium(id.HasValue ? (int)id : 1);
+
+            IList<Ba_Medium> allmodel = dal.Getmedium();
+            int pagenum = 1;
+            if (allmodel.Count % 3 == 0)
+                pagenum = allmodel.Count / 3;
+            else
+                pagenum = allmodel.Count / 3 + 1;
+            int currentpage = id.HasValue ? (int)id : 1;
+            Common.HtmlPagerControl page = new Common.HtmlPagerControl(pagenum, 3, 7);
+            page.CurrentPage = currentpage;
+            ViewBag.currentpage = currentpage;
+            page.ClickEvent = "onclick=\"topage(this);\"";
+            ViewBag.pageinfo = page.Render();
 
             return View(model);
         }
