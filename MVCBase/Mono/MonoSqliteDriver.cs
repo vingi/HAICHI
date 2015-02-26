c62e40a3e0f19c33772e22559282e33ea9b4b2bb
vingi <vingi520@msn.com>
2015-02-27 03:12:20 +0800

    Create MONO Version

---
+++ MVCBase/Mono/MonoSqliteDriver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBase
+{
+    public class MonoSqliteDriver : NHibernate.Driver.ReflectionBasedDriver
+    {
+        public MonoSqliteDriver() :
+            base("Mono.Data.Sqlite",
+            "Mono.Data.Sqlite.SqliteConnection",
+            "Mono.Data.Sqlite.SqliteCommand")
+        {
+        }
+        public override bool UseNamedPrefixInParameter
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override bool UseNamedPrefixInSql
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public override string NamedPrefix
+        {
+            get
+            {
+                return "@";
+            }
+        }
+        public override bool SupportsMultipleOpenReaders
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }  
+}
